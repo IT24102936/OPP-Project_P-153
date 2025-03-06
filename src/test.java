d02ee767725822eefa545cc1c0a821049492505b
IT24102936 <it24102936@my.sliit.lk>
2025-03-06 18:34:38 +0530

    Update test.javas

--- src/test.java
+++ src/test.java
@@ -1,5 +1,5 @@
 public class test {
     public static void main(String[] args) {
-        System.out.println("Hsdsdello World");
+        System.out.println("Hsdsdello Worlssssssssssssssssssssssssssssssssd");
     }
 }
