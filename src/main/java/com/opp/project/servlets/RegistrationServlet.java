edd45417b6f63476a141a80e6c063875e66d1571
IT24102898 <it24102898@my.sliit.lk>
2025-03-26 12:10:40 +0530

    Add password validation function to registration form

--- src/main/java/com/opp/project/servlets/RegistrationServlet.java
+++ src/main/java/com/opp/project/servlets/RegistrationServlet.java
@@ -52,4 +52,7 @@ public class RegistrationServlet extends HttpServlet {
         response.setContentType("text/html");
         response.getWriter().println("<html><body><h2>Registration Successful!</h2></body></html>");
     }
+
+
+
 }
